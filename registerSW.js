28463f7652c69562d082f260956e83209c2d2150
Sergey Volkov <js2me@outlook.com>
2024-12-31 10:12:51 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://js2me.github.io/time-tracker-app/sw.js', { scope: 'https://js2me.github.io/time-tracker-app/' })})}
