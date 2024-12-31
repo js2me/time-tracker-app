a468bc9ac8a9d5e69115c076934ab667be4ee1f5
Sergey Volkov <js2me@outlook.com>
2024-12-31 10:18:28 +0300

    Updates

--- assets/timer-worker-1EJ-NBO1.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";let t,s=!1;const i=e=>{t=setTimeout(()=>{s&&(self.postMessage({type:"tick"}),i(e))},e)};self.onmessage=({data:e})=>{e.type==="start"&&(clearTimeout(t),s=!0,i(e.delay)),e.type==="stop"&&(s=!1,clearTimeout(t))}})();
