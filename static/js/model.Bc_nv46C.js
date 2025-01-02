57754285447136570d6c3c1ca7155581e0822da0
Sergey Volkov <js2me@outlook.com>
2025-01-02 20:51:55 +0300

    Updates

---
+++ static/js/model.Bc_nv46C.js
@@ -0,0 +1,1 @@
+import{P as e}from"./bundle.gU2dM5lW.js";class m extends e{mount(){super.mount(),document.title="О проекте"}}export{m as AboutPageVM};
--- static/js/model.HAmgoi0i.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./bundle.DKf7s84F.js";class m extends e{mount(){super.mount(),document.title="О проекте"}}export{m as AboutPageVM};
