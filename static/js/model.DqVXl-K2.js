8b17f68059999654879c5e1aba12022242723622
Sergey Volkov <js2me@outlook.com>
2025-01-03 08:52:07 +0300

    Updates

---
+++ static/js/model.DqVXl-K2.js
@@ -0,0 +1,1 @@
+import{P as e}from"./bundle.BX9lCCQe.js";class m extends e{mount(){super.mount(),document.title="О проекте"}}export{m as AboutPageVM};
--- static/js/model.M9E7LogI.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./bundle.BQew23eP.js";class m extends e{mount(){super.mount(),document.title="О проекте"}}export{m as AboutPageVM};
