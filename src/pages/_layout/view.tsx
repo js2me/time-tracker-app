cded80f864d94a5b1e3943632f617730431e06cb
Sergey Volkov <js2me@outlook.com>
2025-01-03 11:02:06 +0300

    refactor: LayoutView component

--- src/pages/_layout/view.tsx
+++ src/pages/_layout/view.tsx
@@ -14,69 +14,56 @@ import { LayoutVM } from './model';
 interface LayoutViewProps extends ViewModelProps<LayoutVM> {
   className?: string;
   children?: ReactNode;
-  customLogoClick?: VoidFunction;
 }
 
-export const LayoutView = observer(
-  ({ children, customLogoClick, model }: LayoutViewProps) => {
-    const { theme } = useRootStore();
+export const LayoutView = observer(({ children, model }: LayoutViewProps) => {
+  const { theme } = useRootStore();
 
-    const handleLogoClick = () => {
-      if (customLogoClick) {
-        return customLogoClick();
-      }
-
-      model.handleClickLogo();
-    };
-
-    return (
+  return (
+    <div
+      className={cx(
+        'relative mx-auto min-h-full py-10 flex flex-col max-lg:px-6 max-md:py-6 max-xs:p-4 max-w-[640px]',
+      )}
+    >
       <div
-        className={cx(
-          'relative mx-auto min-h-full py-10 flex flex-col max-lg:px-6 max-md:py-6 max-xs:p-4 max-w-[640px]',
-        )}
+        className={
+          'mb-5 flex flex-row items-center justify-start gap-4 lg:mb-10'
+        }
       >
-        <div
-          className={
-            'mb-5 flex flex-row items-center justify-start gap-4 lg:mb-10'
-          }
-        >
-          <img
-            alt={'Тайм трекер машина'}
-            src={logoImageUrl}
-            onClick={handleLogoClick}
-            className={`size-16 rounded-sm text-reverse max-xs:size-10 cursor-pointer ${
-              model.isLogoAnimating ? 'animate-pulse duration-1000' : ''
-            }`}
-          />
-          <div className={'relative'}>
-            <h1
-              className={
-                'cursor-pointer text-3xl font-extrabold tracking-tight'
-              }
-              onClick={handleLogoClick}
-            >
-              Тайм Трекер Машина
-            </h1>
-            <Link
-              href={'/about'}
-              className={'text-xs absolute -bottom-3.5 left-0 text-link'}
-            >
-              О проекте
-            </Link>
-          </div>
-          <Button
-            variant={'ghost'}
-            size={'icon'}
-            className={'ml-auto'}
-            onClick={theme.switchTheme}
+        <img
+          alt={'Тайм трекер машина'}
+          src={logoImageUrl}
+          onClick={model.handleClickLogo}
+          className={`size-16 rounded-sm text-reverse max-xs:size-10 cursor-pointer ${
+            model.isLogoAnimating ? 'animate-pulse duration-1000' : ''
+          }`}
+        />
+        <div className={'relative'}>
+          <h1
+            className={'cursor-pointer text-3xl font-extrabold tracking-tight'}
+            onClick={model.handleClickLogo}
           >
-            {theme.theme === 'light' && <Sun />}
-            {theme.theme === 'auto' && <SunMoon />}
-            {theme.theme === 'dark' && <Moon />}
-          </Button>
+            Тайм Трекер Машина
+          </h1>
+          <Link
+            href={'/about'}
+            className={'text-xs absolute -bottom-3.5 left-0 text-link'}
+          >
+            О проекте
+          </Link>
         </div>
-        <div className={'flex flex-col relative'}>{children}</div>
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className={'ml-auto'}
+          onClick={theme.switchTheme}
+        >
+          {theme.theme === 'light' && <Sun />}
+          {theme.theme === 'auto' && <SunMoon />}
+          {theme.theme === 'dark' && <Moon />}
+        </Button>
       </div>
-    );
-  },
-);
+      <div className={'flex flex-col relative'}>{children}</div>
+    </div>
+  );
+});
