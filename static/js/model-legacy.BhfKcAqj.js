480daeae18c63f31a4679de130ced5c46931c876
Sergey Volkov <js2me@outlook.com>
2025-01-11 00:56:19 +0300

    Updates

--- static/js/model-legacy.BhfKcAqj.js
+++
@@ -1,1 +0,0 @@
-System.register(["./bundle-legacy.Dep6MZxN.js"],(function(e,t){"use strict";var s;return{setters:[e=>{s=e.P}],execute:function(){e("AboutPageVM",class extends s{mount(){super.mount(),document.title="О проекте"}})}}}));
