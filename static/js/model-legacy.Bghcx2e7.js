8fae0c55849a67860ee02b43f44cded7110b70cd
Sergey Volkov <js2me@outlook.com>
2025-01-27 17:57:15 +0300

    Updates

--- static/js/model-legacy.Bghcx2e7.js
+++
@@ -1,1 +0,0 @@
-System.register(["./bundle-legacy.CAuN4qXj.js"],(function(e,t){"use strict";var s;return{setters:[e=>{s=e.P}],execute:function(){e("AboutPageVM",class extends s{mount(){super.mount(),document.title="О проекте"}})}}}));
---
+++ static/js/model.CsFdRUne.js
@@ -0,0 +1,1 @@
+import{P as e}from"./bundle.CFNrt52u.js";class m extends e{mount(){super.mount(),document.title="О проекте"}}export{m as AboutPageVM};
